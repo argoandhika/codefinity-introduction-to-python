30672dc9104962382601e85e73c4587dd6c3216a
argoandhika <>
2026-01-06 02:28:43 +0000

    String Manipulation

--- variables_and_types/string_manipulation/main.py
+++ variables_and_types/string_manipulation/main.py
@@ -1,1 +1,9 @@
 grocery_items = "milk cheese bread apples oranges chicken"
+
+# Ekstrak item menggunakan string slicing
+dairy1 = grocery_items[0:4]    # "milk"
+dairy2 = grocery_items[5:11]   # "cheese"
+bakery1 = grocery_items[12:17] # "bread"
+
+# Buat dan cetak pesan sesuai format yang diminta
+print("We have dairy and bakery items:", dairy1 + ",", dairy2 + ", and", bakery1, "in aisle 5")
