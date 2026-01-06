a20b561dcebd6c1d82bc6d624f0aa66d80c805f2
argoandhika <>
2026-01-06 03:08:54 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,9 @@
+# Definisikan variabel total_cost
+total_cost = 25.00
+
+# Buat variabel boolean untuk memeriksa kelayakan diskon
+# total_cost >= 20.00 akan menghasilkan True karena 25.00 > 20.00
+discountEligible = total_cost >= 20.00
+
+# Cetak hasil sesuai format yang diminta
+print("Is the purchase eligible for a discount?", discountEligible)
