df7330a79a08939708cebf0b04faf7b390be8fa9
argoandhika <>
2026-01-20 07:50:25 +0000

    Challenge: Product Revenues Capstone

--- functions/challenge_product_revenues_capstone/main.py
+++ functions/challenge_product_revenues_capstone/main.py
@@ -1,7 +1,19 @@
 # List of products, their prices, and the quantities sold
 products = ["Bread", "Apples", "Oranges", "Bananas"]
-prices = [0.50, 1.20, 2.50, 2.00]  # price per item
-quantities_sold = [150, 200, 100, 50]  # number of items sold
+prices = [0.50, 1.20, 2.50, 2.00]
+quantities_sold = [150, 200, 100, 50]
 
-# Example of expected output line (do not remove):
-print(f"{revenue[0]} has total revenue of ${revenue[1]}")
+def calculate_revenue(prices, quantities_sold):
+    revenue = []
+    for i in range(len(prices)):
+        revenue.append(prices[i] * quantities_sold[i])
+    return revenue
+
+def formatted_output(revenues):
+    sorted_revenues = sorted(revenues)
+    for revenue in sorted_revenues:
+        print(f"{revenue[0]} has total revenue of ${revenue[1]}")
+
+revenue = calculate_revenue(prices, quantities_sold)
+revenue_per_product = list(zip(products, revenue))
+formatted_output(revenue_per_product)
